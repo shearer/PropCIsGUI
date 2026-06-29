7ed9d0b82f784c36536ffef0fdbb879abc96a1ad
Ralph Scherer <shearer.ra76@gmail.com>
2026-06-29 20:30:09 +0200

    changes for R CMD check as-cran

---
+++ R/PropCIsGUI-package.R
@@ -0,0 +1,9 @@
+#' @keywords internal
+#' @importFrom shiny runApp
+#' @importFrom bslib bs_theme
+#' @importFrom ggplot2 ggplot aes geom_pointrange geom_hline labs theme_bw theme element_text
+#' @importFrom DT datatable DTOutput renderDT
+#' @importFrom patchwork wrap_plots
+#' @importFrom scales percent_format
+#' @importFrom PropCIs scoreci wald2ci orscoreci riskscoreci
+"_PACKAGE"
